3e8039ea005f4f6963591c84f52d558202784195
Giovanni Luz Monteiro <96702007+giovannilm3755@users.noreply.github.com>
2022-04-09 13:36:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let h1 = document.getElementsByTagName('h1')[0]
+h1.innerHTML = 'PARTICIPE DO WORKSHOP QUE MUDARÁ O SEU NEGOCIO'
+
+let h3 = document.getElementsByTagName('h3')[0]
+h3.innerHTML = 'GARANTA SEU LUGAR NO WORKSHOP DO MAIOR ESPECIALISTA EM MARKETING DA ATUALIDADE'
+
+let p = document.getElementsByTagName('p')[0]
+p.innerHTML = 'Neste evento, você irá aprender todos os segredos que vão te fazer ser um empreendedor de sucesso sem precisar contar com a sorte'
+
+let whoIs = document.querySelector('#whoIs')
+whoIs.innerText = 'Victor O Viking'
+
+let description = document.querySelector('#description')
+description.innerText = 'Formado na Universidade de marte e com PHD em fritação (Música eletrônica), Victor está aqui para te levar para o espaço com suas ideias!'
+
